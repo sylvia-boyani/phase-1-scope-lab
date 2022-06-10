3456ea93b67b4134945c0607e0e5a2011e03983b
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-10 11:28:50 +0300

    commit tree c22e16ca41cd12a4ae20d12826cd9b0ba78d4d68

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+var customerName = "bob"
+console.log(customerName)
+
+ upperCaseCustomerName(customerName)
+ customerName = "bob"
+    console.log(customerName.toUpperCase());
+
+
+
